0fafed7f25ba287531ba2fa0bca7b2b4e9fef69e
kendalldirks <kendalldirks7@gmail.com>
2026-05-04 20:31:47 -0500

    preview_animation

--- nodes/image_nodes.py
+++ nodes/image_nodes.py
@@ -519,3 +519,84 @@ class PreviewImageKD(SaveImage):
     FUNCTION = "save_images"
     OUTPUT_NODE = True
     CATEGORY = "KDNodes/image"
+
+class PreviewAnimationKD:
+    def __init__(self):
+        self.output_dir = folder_paths.get_temp_directory()
+        self.type = "temp"
+        self.prefix_append = "_temp_" + ''.join(random.choice("abcdefghijklmnopqrstupvxyz") for x in range(5))
+        self.compress_level = 1
+
+    methods = {"default": 4, "fastest": 0, "slowest": 6}
+    @classmethod
+    def INPUT_TYPES(s):
+        return {"required":
+                    {
+                     "fps": ("FLOAT", {"default": 8.0, "min": 0.01, "max": 1000.0, "step": 0.01}),
+                     },
+                "optional": {
+                    "images": ("IMAGE", ),
+                    "masks": ("MASK", ),
+                },
+            }
+
+    RETURN_TYPES = ("IMAGE", "MASK")
+    RETURN_NAMES = ("images", "masks")
+    FUNCTION = "preview"
+    OUTPUT_NODE = True
+    CATEGORY = "KDNodes/image"
+
+    def preview(self, fps, images=None, masks=None):
+        filename_prefix = "AnimPreview"
+        full_output_folder, filename, counter, subfolder, filename_prefix = folder_paths.get_save_image_path(filename_prefix, self.output_dir)
+        results = list()
+
+        pil_images = []
+
+        if images is not None and masks is not None:
+            for image in images:
+                i = 255. * image.cpu().numpy()
+                img = Image.fromarray(np.clip(i, 0, 255).astype(np.uint8))
+                pil_images.append(img)
+            for mask in masks:
+                if pil_images:
+                    mask_np = mask.cpu().numpy()
+                    mask_np = np.clip(mask_np * 255, 0, 255).astype(np.uint8)
+                    mask_img = Image.fromarray(mask_np, mode='L')
+                    img = pil_images.pop(0)
+                    img = img.convert("RGBA")
+                    rgba_mask_img = Image.new("RGBA", img.size, (255, 255, 255, 255))
+                    rgba_mask_img.putalpha(mask_img)
+                    composited_img = Image.alpha_composite(img, rgba_mask_img)
+                    pil_images.append(composited_img)
+
+        elif images is not None and masks is None:
+            for image in images:
+                i = 255. * image.cpu().numpy()
+                img = Image.fromarray(np.clip(i, 0, 255).astype(np.uint8))
+                pil_images.append(img)
+
+        elif masks is not None and images is None:
+            for mask in masks:
+                mask_np = 255. * mask.cpu().numpy()
+                mask_img = Image.fromarray(np.clip(mask_np, 0, 255).astype(np.uint8))
+                pil_images.append(mask_img)
+        else:
+            print("PreviewAnimation: No images or masks provided")
+            return {"ui": {"images": results, "animated": (None,), "text": "empty"}, "result": (None, None)}
+
+        num_frames = len(pil_images)
+
+        c = len(pil_images)
+        for i in range(0, c, num_frames):
+            file = f"{filename}_{counter:05}_.webp"
+            pil_images[i].save(os.path.join(full_output_folder, file), save_all=True, duration=int(1000.0/fps), append_images=pil_images[i + 1:i + num_frames], lossless=False, quality=50, method=0)
+            results.append({
+                "filename": file,
+                "subfolder": subfolder,
+                "type": self.type
+            })
+            counter += 1
+
+        animated = num_frames != 1
+        return {"ui": {"images": results, "animated": (animated,), "text": [f"{num_frames}x{pil_images[0].size[0]}x{pil_images[0].size[1]}"]}, "result": (images, masks)}
